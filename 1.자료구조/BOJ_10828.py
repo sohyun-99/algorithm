f9a631ae1b199b1c5fff3c7c54dab881390a59ea
sohyun-99 <sophia4130@naver.com>
2024-01-02 22:41:04 +0900

    [ADD] BOJ_10828 (23.01.02)

---
+++ 1.자료구조/BOJ_10828.py
@@ -0,0 +1,27 @@
+import sys
+
+stack = []
+
+n = int(sys.stdin.readline().rstrip())
+
+for _ in range(n):
+    command = sys.stdin.readline().split()
+    if command[0] == "push":
+        stack.append(int(command[1]))
+    elif command[0] == "pop":
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == "size":
+        print(len(stack))
+    elif command[0] == "empty":
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif command[0] == "top":
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
