7e23d34318d93b23a57428ca75690887a1ceb636
sohyun-99 <sophia4130@naver.com>
2024-01-03 23:42:17 +0900

    [ADD] 이코테_국영수 (24.01.03)

---
+++ 5.정렬/이코테_국영수.py
@@ -0,0 +1,13 @@
+# Q23 국영수
+
+n = int(input())
+
+arr = []
+
+for _ in range(n) :
+  arr.append(input().split())
+
+arr.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+for student in arr :
+  print(student[0])
