92c6321e4a562c44db1220dedb25cc8d8548f779
sohyun-99 <sophia4130@naver.com>
2024-01-28 22:41:42 +0900

    [ADD] BOJ_1260 (24.01.28)

---
+++ java_coding/data_structure/bfs_dfs/BOJ_1260.java
@@ -0,0 +1,71 @@
+package java_coding.data_structure.bfs_dfs;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1260 {
+
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken()); 
+
+        ArrayList<Integer>[] graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        boolean[] visited = new boolean[n + 1];
+        dfs(v);
+        System.out.println();
+
+        visited = new boolean[n + 1];
+        bfs(v);
+    }
+
+    static void dfs(int node) {
+        visited[node] = true;
+        System.out.print(node + " ");
+
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+    static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        visited[start] = true;
+        queue.offer(start);
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            System.out.print(current + " ");
+
+            for (int next : graph[current]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.offer(next);
+                }
+            }
+        }
+    }
+}
