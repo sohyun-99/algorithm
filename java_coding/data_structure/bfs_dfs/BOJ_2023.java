a56174f1447ec879c8b4b070d5ccab972af97241
sohyun-99 <sophia4130@naver.com>
2024-02-01 23:58:35 +0900

    [UPDATE] BOJ_2023

--- java_coding/data_structure/bfs_dfs/BOJ_2023.java
+++ java_coding/data_structure/bfs_dfs/BOJ_2023.java
@@ -6,54 +6,51 @@ import java.io.*;
 // 신기한 소수 찾기
 public class BOJ_2023 {
 
-    private static ArrayList<Integer> result = new ArrayList<>();
+    // private static ArrayList<Integer> result = new ArrayList<>();
     private static int n ;
-    private static int[] start = {1,2,3,5,7};
+    private static int[] start = {2,3,5,7};
     private static int[] next = {1,3,5,7,9};
 
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
-
         n = sc.nextInt();
-
-        for(int i=0 ; i<5 ; i++){
-            result.add(start[i]);
+        for(int i=0 ; i<4 ; i++){
+            // result.add(start[i]);
             cal(start[i]); // 첫번째 수를 정해서 보냄
         
         }
-
-
     }
 
     //부분이 소수인 것만 num에 들어옴
     private static int cal(int num){
 
-        System.out.println("들어옴"+num);
+        int num2=0;
 
-        if (Integer.toString(num).length()==n){
-            System.out.println(num);
-            result.add(num);
-            return 0;
-        }
-        else if(Integer.toString(num).length()<n){
-            if(isPrime(num)){
-                for(int j=0;j<5;j++){
-                    num = num * 10 +next[j];
-                    cal(num);
-                }
+        if(isPrime(num)){
+            if(Integer.toString(num).length()<n){
+                {
+                    for(int j=0;j<5;j++){
+                        if(isPrime(num)){
+                        num2 = num * 10 +next[j];
+                        cal(num2);
+                        }
+                    }
+                }   
             }
+            if (Integer.toString(num).length()==n){
+                System.out.println(num);
+                // result.add(num);
+                return 0;
+            }
+        }  
+        return 0;         
         }
-
-
-        return 0;
-           
-    }
 
     // 소수 판별 함수
     public static boolean isPrime(int num) {
         if (num < 2) {
-            return true;
+            return false;
         }
         for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
@@ -61,8 +58,6 @@ public class BOJ_2023 {
             }
         }
         return true;
-
-    
     }
 }
 
